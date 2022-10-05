1973e70c4a4e20fba516c74a681e0212f39297bb
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-10-05 15:25:12 -0500

    chore: rendering projects list

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,34 @@ function autobind(
   return adjDescriptor;
 }
 
+// ProjectList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = <HTMLTemplateElement> document.getElementById('project-list')!;
+    this.hostElement = <HTMLDivElement> document.getElementById('app')!;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = <HTMLElement> importedNode.firstElementChild;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
 // ProjectInput class
 class ProjectInput {
 templateElement: HTMLTemplateElement;
@@ -130,3 +158,5 @@ peopleInputElement: HTMLInputElement;
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
